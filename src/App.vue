13f7c5d147e6bdef4cbb5b05e7572c6b22a42510
Hamid Kamyab <mr_kamyab@yahoo.com>
2024-06-29 23:31:53 +0330

    Project completion

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "vue";
+  
+  const count = ref(0)
+  const handleCounter = ()=>{
+    count.value++
+    console.log(count.value)
+  }
+</script>
+
+<template>
+    <div class='main'>
+      <h2>{{count}}</h2>
+      <div class="btn-box">
+        <button class="button button-yellow" @click="count=0" >بازنشانی</button>
+        <button class="button button-light" @click="handleCounter" >شمارنده</button>
+      </div>
+    </div>
+</template>
